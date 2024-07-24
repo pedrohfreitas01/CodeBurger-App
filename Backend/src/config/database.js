b03873f91560b96668d3a1fc5544d9c51762332b
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-07-23 21:39:10 -0300

    remove config/database.js to gitignore

---
+++ Backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'lucydormindo',
+    database: 'codeburger',
+    define: {
+        timespamps: true,
+        udnerscored: true,
+        udnerscoredAll: true
+    }
+}
