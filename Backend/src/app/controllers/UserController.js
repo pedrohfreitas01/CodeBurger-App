b7d2a12582746d5c12c121939df1f01b2714e8f9
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-07-28 09:51:38 -0300

    ✨ feat: add Yup library for schema validation

--- Backend/src/app/controllers/UserController.js
+++ Backend/src/app/controllers/UserController.js
@@ -1,11 +1,28 @@
 import { v4 } from "uuid"
 import User from "../models/User";
-
-
+import * as Yup from 'yup'
 
 
 class UserController{
     async store(req, res) {
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().required().min(6),
+            admin: Yup.boolean(),
+        })
+
+        // if (!(await schema.isValid(req.body))) {
+        //     return res.status(400).json({error: "Make sure your data is correct"})
+        // }
+        try {
+            await schema.validateSync(req.body, {abortEarly: false}) //send all the erros , not just one
+        } catch (err) {
+            return res.status(400).json({error: err.errors })
+        }
+        
+
         const { name, email, password_hash, admin } = req.body
         
         const user = await User.create ({
