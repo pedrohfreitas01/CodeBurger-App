523d8c81e6a0c89e6aa417c0ff5e25ad40a8fcf1
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-08-04 09:19:45 -0300

    save orders in MongoDB

--- Backend/src/app/controllers/OrderController.js
+++ Backend/src/app/controllers/OrderController.js
@@ -1,68 +1,74 @@
-import * as Yup from 'yup';
-import Categories from '../models/Categories';
-import Products from '../models/Products';
+import * as Yup from "yup";
+import Categories from "../models/Categories";
+import Products from "../models/Products";
+import Order from "../schemas/Order"
 
 class OrderController {
-    async store(req, res) {
-        const schema = Yup.object().shape({
-            products: Yup.array()
-                .required()
-                .of(
-                    Yup.object().shape({
-                        id: Yup.number().required(),
-                        quantity: Yup.number().required()
-                    })
-                )
-        });
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      products: Yup.array()
+        .required()
+        .of(
+          Yup.object().shape({
+            id: Yup.number().required(),
+            quantity: Yup.number().required(),
+          })
+        ),
+    });
 
-        try {
-            await schema.validateSync(req.body, { abortEarly: false });
-        } catch (err) {
-            return res.status(400).json({ error: err.errors });
-        }
-
-        const productsId = req.body.products.map((product) => product.id);
-
-        // Consultando os produtos no banco de dados
-        const updateProducts = await Products.findAll({
-            where: {
-                id: productsId,
-            },
-            include: [
-                {
-                    model: Categories,
-                    as: 'category',
-                    attributes: ['name'] // Corrigido de 'attibutes' para 'attributes'
-                }
-            ]
-        });
-
-        // Usando updateProducts em vez de filteredProducts
-        const editedProduct = updateProducts.map((product) => {
-            const productIndex = req.body.products.findIndex((reqProduct) => reqProduct.id === product.id);
-
-            const newProduct = {
-                id: product.id,
-                name: product.name,
-                price: product.price,
-                category: product.category.name,
-                url: product.url,
-                quantity: req.body.products[productIndex].quantity,
-            };
-
-            return newProduct;
-        });
-
-        const order = {
-            user: {
-                id: req.userId,
-                name: req.userName,
-            },
-            products: editedProduct,
-        };
-
-        return res.status(201).json(editedProduct); // Retornando o objeto 'order'
+    try {
+      await schema.validateSync(req.body, { abortEarly: false });
+    } catch (err) {
+      return res.status(400).json({ error: err.errors });
     }
+
+    const productsId = req.body.products.map((product) => product.id);
+
+    // Consultando os produtos no banco de dados
+    const updateProducts = await Products.findAll({
+      where: {
+        id: productsId,
+      },
+      include: [
+        {
+          model: Categories,
+          as: "category",
+          attributes: ["name"], // Corrigido de 'attibutes' para 'attributes'
+        },
+      ],
+    });
+
+    const editedProduct = updateProducts.map((product) => {
+      const productIndex = req.body.products.findIndex(
+        (reqProduct) => reqProduct.id === product.id
+      );
+
+      return {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        category: product.category.name,
+        url: product.url,
+        quantity: req.body.products[productIndex].quantity,
+      };
+    });
+
+    const order = {
+      user: {
+        id: req.userId,
+        name: req.userName,
+      },
+      products: editedProduct,
+      status: "Pedido realizado",
+    };
+
+    try {
+      const orderResponse = await Order.create(order);
+      return res.status(201).json(orderResponse); // Retornando o objeto 'order'
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
 }
 
 export default new OrderController();
--- Backend/src/app/schemas/Order.js
+++ Backend/src/app/schemas/Order.js
@@ -4,41 +4,49 @@ const OrderSchema = new mongoose.Schema(
   {
     user: {
       id: {
-        type: String, 
-        required: true
+        type: String,
+        required: true,
       },
       name: {
         type: String,
-        required: true
+        required: true,
       },
-      products: [
-        {
-          id: {
-            type: Number,
-            required: true
-          },
-          name: {
-            type: String,
-            required: true
-          },
-          price: {
-            type: Number,
-            required: true
-          },
-          category: {
-            type: Number,
-            required: true
-          }
-        }
-      ],
-      status: {
-        type: String,
-        required: true
-      }
-    }
+    },
+    products: [
+      {
+        id: {
+          type: Number,
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        category: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      required: true,
+    },
   },
   {
-    timestamps: true 
+    timestamps: true,
   }
 );
 
