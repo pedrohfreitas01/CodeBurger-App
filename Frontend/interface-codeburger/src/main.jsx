05e347b7d3d1795dd85a0f377c9ed4116d784fec
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-08-05 13:40:21 -0300

    🚀 feat: inicialização do projeto

---
+++ Frontend/interface-codeburger/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Login from './containers/Login'
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login></Login>    
+  </React.StrictMode>,
+)
