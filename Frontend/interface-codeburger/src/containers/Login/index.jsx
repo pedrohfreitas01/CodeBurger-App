0e75220ee7f918076abc2f4b0293ad073ba85a7c
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-08-07 21:22:28 -0300

    feat: login page styled

--- Frontend/interface-codeburger/src/containers/Login/index.jsx
+++ Frontend/interface-codeburger/src/containers/Login/index.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+
+import LoginImg from "../../assets/loginImgBurger.svg";
+import LogoBurger from "../../assets/logoBurgerCode.svg";
+
+import {
+  Container,
+  ImageBurger,
+  ContainerItens,
+  Label,
+  Input,
+  Button,
+  SignInLink,
+} from "./style";
 
 function Login() {
   return (
-    <div>
-      <h1asdasd>asdas doda do asdasd asdp2p1 do </h1asdasd>loign dbrasd asdmebem
-      vindo ao site hahah dsd asdd asdasd
-    </div>
+    <Container>
+      <ImageBurger src={LoginImg} alt="login-image"></ImageBurger>
+      <ContainerItens>
+        <img src={LogoBurger} alt="logo-burger"></img>
+        <h2>Login</h2>
+
+        <Label>Email</Label>
+        <Input />
+
+        <Label>Senha</Label>
+        <Input />
+
+        <Button>Sign In</Button>
+        <SignInLink>
+          Nao possui conta? <a>Sing UP</a>{" "}
+        </SignInLink>
+      </ContainerItens>
+    </Container>
   );
 }
 
--- Frontend/interface-codeburger/src/main.jsx
+++ Frontend/interface-codeburger/src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import Login from './containers/Login'
+import GlobalStyles from './styles/globalStyles'
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login></Login>    
+    <>
+      <Login></Login>
+      <GlobalStyles></GlobalStyles>
+    </>
   </React.StrictMode>,
 )
