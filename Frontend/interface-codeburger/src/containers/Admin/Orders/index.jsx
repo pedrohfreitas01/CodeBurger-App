dd76e7b0073ea6759376263daf03a53d50b121cc
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-08-29 21:05:37 -0300

    :construction: Admin interface

--- Frontend/interface-codeburger/src/containers/Admin/Orders/index.jsx
+++ Frontend/interface-codeburger/src/containers/Admin/Orders/index.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
+import api from "../../../services/api";
+import Row from "./row";
 
 function Orders() {
+  const [orders, setOrders] = useState([]);
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    async function loadOrder() {
+      try {
+        const { data } = await api.get("orders");
+
+        console.log(orders);
+
+        setOrders(data);
+      } catch (error) {
+        console.error("Error load order:", error);
+      }
+    }
+
+    loadOrder();
+  }, []);
+
+  function createData(order) {
+    return {
+      name: order.user.name,
+      orderId: order._id,
+      date: order.createdAt,
+      status: order.status,
+      products: order.products,
+    };
+  }
+
+  useEffect(() => {
+    const newRows = orders.map((ord) => createData(ord));
+    setRows(newRows);
+  }, [orders]);
+
   return (
-    <div>Orders</div>
-  )
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Order</TableCell>
+            <TableCell align="right">Costumer</TableCell>
+            <TableCell align="right">Order Date</TableCell>
+            <TableCell align="right">Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.orderId} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
-export default Orders
+export default Orders;
--- Frontend/interface-codeburger/src/containers/Admin/index.jsx
+++ Frontend/interface-codeburger/src/containers/Admin/index.jsx
@@ -2,58 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Container } from "./style";
 import Orders from "./Orders";
 
-import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-
-import api from "../../services/api";
-
 export function Admin() {
-  const [orders, setOrders] = useState([]);
-  const [rows, setRows] = useState([]);
-
-  useEffect(() => {
-    async function loadOrder() {
-      try {
-        const { data } = await api.get("orders");
-
-        console.log(orders);
-
-        setOrders(data);
-      } catch (error) {
-        console.error("Error load order:", error);
-      }
-    }
-
-    loadOrder();
-  }, []);
-
-  function createData(order) {
-    return {
-      name: order.user.name,
-      orderId: order._id,
-      date: order.createdAt,
-      status: order.status,
-      products: order.products,
-    };
-  }
-
-  useEffect(() => {
-    const newRows = orders.map((ord) => createData(ord));
-    setRows(newRows);
-  }, [orders]);
-
-  console.log(rows);
   return (
     <Container>
       <Orders />
