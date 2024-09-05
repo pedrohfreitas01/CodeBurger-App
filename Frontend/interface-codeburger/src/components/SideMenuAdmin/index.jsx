c1968059051fe81ac03351900083cdea7a686f76
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-09-04 21:25:31 -0300

    feat: admin logout

--- Frontend/interface-codeburger/src/components/SideMenuAdmin/index.jsx
+++ Frontend/interface-codeburger/src/components/SideMenuAdmin/index.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { Container, ItemContainer, ListLink } from "./style";
 import listMenu from "./menu-list";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useUser } from "../../hooks/UserContext";
 
 export function SideMenuAdm() {
+  const { logout } = useUser();
+
   return (
     <Container>
       <hr />
       {listMenu.map((item) => (
-        <ItemContainer key={item.id}>
-          <item.icon className="icon"/>
-          <ListLink to={item.link}>{ item.label}</ListLink>
+        <ItemContainer key={item.id} isActive={true}>
+          <item.icon className="icon" />
+          <ListLink to={item.link}>{item.label}</ListLink>
         </ItemContainer>
       ))}
+      <hr />
+      <ItemContainer style={{ position: "absolute", bottom: "30px" }}>
+        <LogoutIcon style={{ color: "#fffff" }} />
+        <ListLink to="/login" onClick={logout}>
+          Sair
+        </ListLink>
+      </ItemContainer>
     </Container>
   );
 }
